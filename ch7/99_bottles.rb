9557a7850bb60f2300862e2c208d17e08a3e6fc8
Selma Muminovic <muminovic@Selmas-MacBook-Air.local>
2017-01-05 14:18:56 -0800

    made alterations

--- ch7/99_bottles.rb
+++ ch7/99_bottles.rb
@@ -1,7 +1,6 @@
 number = 99
 while number > 1
-	puts number.to_s + ' bottles of beer on the wall, ' 
-	puts number.to_s + ' bottles of beer!'
+	puts number.to_s + ' bottles of beer on the wall, ' + number.to_s + ' bottles of beer!'
 	puts 'Take one down and pass it around,'
 	number = number - 1
 	if number != 1
@@ -10,13 +9,9 @@ while number > 1
 		puts number.to_s + ' bottle of beer on the wall!' 
 	end	
 if number == 1
-	puts number.to_s + ' bottle of beer on the wall, '
-	puts number.to_s + ' bottle of beer!'
-	puts 'Take one down and pass it around, '
-	puts 'no more bottles of beer on the wall!'
-	puts 'No more bottles of beer on the wall, '
-	puts 'no more bottles of beer.' 
-	puts 'Go to the store and buy some more, '
-	puts '99 bottles of beer on the wall!'
+	puts number.to_s + ' bottle of beer on the wall, ' + number.to_s + ' bottles of beer!'
+	puts 'Take one down and pass it around, no more bottles of beer on the wall!'
+	puts 'No more bottles of beer on the wall, no more bottles of beer.'
+	puts 'Go to the store and buy some more, 99 bottles of beer on the wall!'
 	end
 end
