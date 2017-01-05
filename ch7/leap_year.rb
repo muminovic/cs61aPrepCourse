bfeaab61d6716367d5b0f8bef250727890c41d21
Selma Muminovic <muminovic@Selmas-MacBook-Air.local>
2017-01-05 14:14:56 -0800

    made alterations

--- ch7/leap_year.rb
+++ ch7/leap_year.rb
@@ -4,12 +4,9 @@ puts 'Please choose an ending year.'
 finish = gets.chomp
 n = start.to_i
 while n <= finish.to_i
-	if n % 400 == 0
+	if n % 400 == 0 || (n % 100 != 0 && n % 4 == 0)
 		puts n
 		n += 1
-	elsif n % 100 != 0 && n % 4 == 0
-		puts n
-		n +=1
 	end
 n +=1		
 end	
