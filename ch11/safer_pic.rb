2f8c5b9b51bb2ce8c34f42239aec26afbd0cef1a
Selma Muminovic <muminovic@Selmas-MacBook-Air.local>
2017-01-05 22:25:49 -0800

    a few things to try

---
+++ ch11/safer_pic.rb
@@ -0,0 +1,39 @@
+Dir.chdir 'C:/Documents and Settings/Katy/PictureInbox'
+
+pic_names = Dir['F:/**/*.jpg']
+
+puts 'What would you like to call this batch?'
+batch_name = gets.chomp
+
+puts
+print "Downloading #{pic_names.length} files:  "
+
+pic_number = 1
+
+pic_names.each do |name|
+	print '.' 
+	new_name = if pic_number < 10 
+		"#{batch_name}0#{pic_number}.jpg"
+	else
+		"#{batch_name}#{pic_number}.jpg" 
+	end
+
+if new_name.exist?
+	print "This name is already taken. Are you sure you want to overwrite this file?"
+	reply = gets.chomp
+	if reply == yes
+		print "Rewriting file..."
+		File.rename name, new_name
+	elsif reply == no
+		"Renaming process terminated."
+		exit
+	else
+		print "Please reply 'yes' or 'no'."	
+	end
+end		
+
+pic_number = pic_number + 1	
+end
+
+puts
+puts 'Done!'
