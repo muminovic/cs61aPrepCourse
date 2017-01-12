b5c83184328e0564751956ad6f94583c0d6c48e5
Selma Muminovic <muminovic@Selmas-MacBook-Air.local>
2017-01-12 15:33:51 -0800

    updated

--- ch10/shuffle.rb
+++ ch10/shuffle.rb
@@ -1,17 +1,13 @@
 def shuffle array
 	shuffled = []
-	while shuffled.length != array.length
-		array.each do |word|
-			i = rand(array.length)
-			shuffled[i] = word		
-		end
-	end	
-	if shuffled.include?(nil)
-		shuffle(array)		
-	else
-		puts shuffled
-	end		
-end
+	length = array.length
+	while shuffled.length != length
+		index = rand(array.length)
+		shuffled.push(array[index])
+		array.delete_at(index)
+	end
+	puts shuffled
+end		
 
 array = ['1', '2', '3', '4']
 shuffle(array)
