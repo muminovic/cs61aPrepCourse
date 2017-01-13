874a1dc256de6b01c9fe4030ca619b3e1cd4f915
Selma Muminovic <muminovic@Selmas-MacBook-Air.local>
2017-01-12 16:05:11 -0800

    updated

--- ch10/99_bob.rb
+++ ch10/99_bob.rb
@@ -92,7 +92,7 @@ while number > 1
 		puts english_number(number).capitalize + ' bottle of beer on the wall!' 
 	end	
 if number == 1
-	puts english_number(number).capitalize + ' bottle of beer on the wall, ' + english_number(number).capitalize + ' bottles of beer!'
+	puts english_number(number).capitalize + ' bottle of beer on the wall, ' + english_number(number) + ' bottle of beer!'
 	puts 'Take one down and pass it around, no more bottles of beer on the wall!'
 	puts 'No more bottles of beer on the wall, no more bottles of beer.'
 	puts 'Go to the store and buy some more, 99 bottles of beer on the wall!'
